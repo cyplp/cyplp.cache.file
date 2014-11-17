cd1544e38a5fa328cf4d4dd8d08053db942a4967
Cyprien Le Pannérer <clepannerer@edd.fr>
2014-11-17 08:42:27 +0100

    storing meta in json file

--- cyplp/cache/file/__init__.py
+++ cyplp/cache/file/__init__.py
@@ -1,6 +1,7 @@
 import os.path
 import os
 import hashlib
+import json
 
 from dogpile.cache.api import CacheBackend, NO_VALUE
 from dogpile.cache.api import CachedValue
@@ -8,7 +9,7 @@ from dogpile.cache.api import CachedValue
 class CacheFileBackend(CacheBackend):
     def __init__(self, arguments):
         self._root = arguments.get('root', '/tmp/cache')
-        self._cache = {}
+        # self._cache = {}
 
         if not os.path.isdir(self._root):
             # todo intermediaire
@@ -22,29 +23,38 @@ class CacheFileBackend(CacheBackend):
         cacheHash = self._computeHash(key)
 
         path = os.path.join(self._root, cacheHash)
+        pathJson = os.path.join(self._root, cacheHash+'.json')
 
-        if not os.path.isfile(path):
+        if not os.path.isfile(path) or not os.path.isfile(pathJson):
             return NO_VALUE
 
-        with open(path, 'rb') as tmp:
-            metadata = self._cache[cacheHash]
+        metadata = {}
 
+        with open(pathJson, 'rb') as tmp:
+            metadata = json.loads(tmp.read())
+
+        with open(path, 'rb') as tmp:
             return CachedValue(tmp.read(), metadata)
 
 
     def set(self, key, value):
         cacheHash = self._computeHash(key)
         path = os.path.join(self._root, cacheHash)
-
+        pathJson = os.path.join(self._root, cacheHash+'.json')
         # import rpdb
         # rpdb.set_trace()
 
         with open(path, 'wb') as tmp:
             tmp.write(value.payload)
 
-        self._cache[cacheHash] = value.metadata
+        with open(pathJson, 'wb') as tmp:
+            tmp.write(json.dumps(value.metadata))
+
 
     def delete(self, key):
         cacheHash = self._computeHash(key)
         path = os.path.join(self._root, cacheHash)
+        pathJson = os.path.join(self._root, cacheHash+'.json')
+
         os.remove(path)
+        os.remove(pathJson)
